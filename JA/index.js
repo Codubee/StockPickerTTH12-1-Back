23c9cee37b79e0caf470899e06d1b815b609f78c
jonathan arsola <jonathan.arsola01@utrgv.edu>
2021-07-15 11:39:18 -0500

    finished the /getAllPeople route. Still has comments of previos attempts.

--- JA/index.js
+++ JA/index.js
@@ -7,17 +7,39 @@ app.use(express.json())
 //must be able to handle incoming details of the person being added. 
 //route name : /addPerson
 //URL : http://java-sample-api-2020.herokuapp.com/addPerson
-
+app.post('/addPerson', function(req, res){
+    console.log('adding a person')
+    axios.post('http://java-sample-api-2020.herokuapp.com/addPerson', req.body)
+    .then(function(apiResponse){
+        res.json({"message":`Person added.`})
+    })
+})
 
 //create a route called : /getAllPeople
 //URL : http://java-sample-api-2020.herokuapp.com/getAllPeople
 app.get('/getAllPeople', function(req, res) {
     console.log("showing all the people")
     //res.json({'message':'showing all the people.'})
-    axios.get('http://java-sample-api-2020.herokuapp.com/getAllPeople', res.body)
-    .then(function(apiResponse){
-        res.json({"message":"Users shown", "Users":`${res.body}`})
-    })
+    axios.get('http://java-sample-api-2020.herokuapp.com/getAllPeople')
+        .then((response) => {
+            const data = response.data
+            res.json({"message":"Users shown", "Users":`${data.json}`})
+            console.log(data)
+        })
+/*
+        .then(function() {
+            res.json({"message":"Users shown", "Users":`${data}`})
+            console.log(data)
+        })
+*/
+/*
+    async function axiosTest() {
+        const axiosResponse = await axios.get('http://java-sample-api-2020.herokuapp.com/getAllPeople')
+        console.log(axiosResponse.json)
+        res.json({"message":"Users shown", "Users":`${axiosResponse.json}`})
+    }
+*/
+
 })
 
 //route name : /deletePerson
