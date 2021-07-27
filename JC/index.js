8716aea17c306c659bfd265670efd9eb283f706d
Jorge Cancino <jorge.cancino101@gmail.com>
2021-07-27 12:46:59 -0500

    cleaned up comments

--- JC/index.js
+++ JC/index.js
@@ -5,14 +5,12 @@ app.use(express.json())
 
 //Create a post route to add a person
 app.post('/addPerson', function(postmanReq, postmanRes) {
-    //we are calling http://java-sample-api-2020.herokuapp.com/addPerson
     console.log("Adding a person...")
     console.log(postmanReq.body)
     axios.post('http://java-sample-api-2020.herokuapp.com/addPerson', postmanReq.body)
     .then(function (apiResponse) {
         postmanRes.status(200).json({"message":"user added"})
     })
-    //catch for errors should be working
     .catch((error) => {
         console.log(error)
         postmanRes.json({"error":"could not add user"})
@@ -21,14 +19,12 @@ app.post('/addPerson', function(postmanReq, postmanRes) {
 
 //Create a route to get all people
 app.get('/getAllPeople', function(postmanReq, postmanRes) {
-    //we are calling http://java-sample-api-2020.herokuapp.com/getAllPeople
     console.log("Showing all people...")
     axios.get('http://java-sample-api-2020.herokuapp.com/getAllPeople')
     .then((response) => {
         console.log(response.data)
         postmanRes.status(200).json({"message":response.data})
     })
-    //catch for errors should be working
     .catch((error) => {
         console.log(error)
         postmanRes.json({"error":"could not get all people"})
@@ -37,17 +33,13 @@ app.get('/getAllPeople', function(postmanReq, postmanRes) {
 
 //Create a route to delete a person
 app.delete('/deletePerson', function(postmanReq, postmanRes) {
-    //this is the URL needed https://java-sample-api-2020.herokuapp.com/deletePerson?id=1
-    //Note: query must change depending on what the user enters (so ?id=1 shouldn't be hardwritten like it is shown, but should be based on
-    //what the user says)
     console.log("Removing a person...")
-    userQuery = postmanReq.query.id
+    var userQuery = postmanReq.query.id
     axios.delete('https://java-sample-api-2020.herokuapp.com/deletePerson?id=' + userQuery)   
     .then((response) => {
         console.log(response.data)
         postmanRes.status(200).json({"message":"user removed"})
     })
-    //catch for errors should be working
     .catch((error) => {
         console.log(error)
         postmanRes.json({"error":"could not remove a person"})
