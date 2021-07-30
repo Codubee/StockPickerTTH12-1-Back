c2088d5dee408b72468522e404b76fce17343026
jonathan arsola <jonathan.arsola01@utrgv.edu>
2021-07-30 13:29:44 -0500

    cleaned the code

--- index.js
+++ index.js
@@ -4,21 +4,8 @@ const axios = require('axios')
 app.use(express.json())
 
 
-//post route: (/stocks/addStock)
-//Only accept when the req is a yes, meaning user select yes to
-//  buying a stock. 
-//url : https://codubee-projects-api.herokuapp.com/stocks/addStock
-/*
- {
-    "userId":"20",
-    "stock":{"companyName":"Microsoft","symbol":"MSFT","image":"","stockId":"19"}
-}
-*/
-app.post('/stocks/addStock', function(req, res){
-    //check if query field is a yes. 
 
-    //handle the 
-    console.log('adding a stock')
+app.post('/stocks/addStock', function(req, res){
     const data = req.body
     const url ='https://codubee-projects-api.herokuapp.com/stocks/addStock'
     axios.post(url, data)
@@ -26,7 +13,6 @@ app.post('/stocks/addStock', function(req, res){
             res.status(200).json({"message" : "Successful"})
         })
         .catch(function(error){
-            console.log("unsuccessful")
             res.json({"message":"Unsuccessful"})
         })
 })
