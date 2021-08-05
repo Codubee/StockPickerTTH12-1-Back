7d7d3c865598331cd252b0a62ca0efa2e2060e51
SameedB <sameedbhatti123@gmail.com>
2021-08-05 12:45:50 -0500

    Update index.js

--- index.js
+++ index.js
@@ -39,12 +39,12 @@ app.delete('/stocks/deleteStock', (postmanRequest, postmanResponse) => {
     axios.delete('https://codubee-projects-api.herokuapp.com/stocks/deleteStock?userId='+userId + '&stockId='+stockId)
     .then((response) => {
         console.log(response.data)
-        res.status(200).json({"Deleted Stock: ":response.data})
+        postmanResponse.status(200).json({"Deleted Stock: ":response.data})
     })
 
     .catch((error) => {
         console.log(error)
-        res.json({"ErrorResponse": "Couldnt Delete Stock"})
+        postmanResponse.json({"ErrorResponse": "Couldnt Delete Stock"})
     })
 });
 
