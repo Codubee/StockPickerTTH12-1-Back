4cdea9ad6bf02e4ab62114c988cf23d8449f9307
GabeL-code <gabriel8lasut@gmail.com>
2021-08-12 12:39:05 -0500

    finsihed get Matches

--- index.js
+++ index.js
@@ -6,19 +6,20 @@ app.use(express.json())
 
 
 app.get('/getMatches', function(req, res){
-    console.log(`ID : ${req.query['id']} added`)  
-    const id = req.query['id']
-    const url = 'https://codubee-projects-api.herokuapp.com/stocks/getMatches?userId='+id
+      
+    const id = req.query['userId']
+   const url = 'https://codubee-projects-api.herokuapp.com/stocks/getMatches?userId='+id
     
     axios.get(url)
         .then((response) => {
-            console.log('response.data')
+            console.log(response.data)
             res.status(200).json({"Matches": response.data})
         })
         .catch((error) => {
             console.log(error);
             res.json({"message": "api call didnt connect"})
         })
+        
 })
 
 
