3eb2f04da33bd7cc4581ee81289a1a5df823b959
Codubee <70549347+Codubee@users.noreply.github.com>
2021-08-17 12:28:23 -0500

    Update index.js

--- index.js
+++ index.js
@@ -23,5 +23,64 @@ app.get('/getMatches', function(req, res){
         
 })
 
+app.get('/getWeather', function(req, res){
+    const url = 'https://codubee-projects-api.herokuapp.com/getWeather'
+    axios.get(url)
+        .then((response) => {
+            console.log(response.data)
+            res.status(200).json({"Temp": response.data})
+        })
+        .catch((error) => {
+            console.log(error);
+            res.json({"Error": "Couldn't load weather"})
+   })
+})
+
+
+app.post('/addStock', function(req, res){
+    const data = req.body
+    const url ='https://codubee-projects-api.herokuapp.com/stocks/addStock'
+    axios.post(url, data)
+        .then(function(response){
+            res.status(200).json({"message" : "Successful"})
+        })
+        .catch(function(error){
+            res.json({"message":"Unsuccessful"})
+
+        })
+})
+
+
+app.delete('/deleteStock', (postmanRequest, postmanResponse) => {
+    const userId = postmanRequest.query.userId
+    const stockId = postmanRequest.query.stockId
+
+    axios.delete('https://codubee-projects-api.herokuapp.com/stocks/deleteStock?userId='+userId + '&stockId='+stockId)
+    .then((response) => {
+        console.log(response.data)
+        postmanResponse.status(200).json({"Deleted Stock: ":response.data})
+    })
+
+    .catch((error) => {
+        console.log(error)
+        postmanResponse.json({"ErrorResponse": "Couldnt Delete Stock"})
+    })
+});
+
+
+app.get('/getStockData', function(postmanReq, postmanRes) {
+    console.log("Getting Data for a Stock...")
+    axios.get('https://codubee-projects-api.herokuapp.com/stocks/getStockData')
+    .then((response) => {
+        console.log(response.data)
+        postmanRes.status(200).json({"Data":response.data})
+    })
+    .catch((error) => {
+        console.log(error)
+        postmanRes.json({"error":"could not get stock data"})
+    })
+})
+
+
 
 app.listen( process.env.PORT || 8080, () => console.log('Example app listening at http://localhost:8080'))
